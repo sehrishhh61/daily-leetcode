17993844ad10ec558ef5542d2193fdeed29111a8
sehrishhh61 <sehrishhh35@gmail.com>
2026-06-08 21:42:54 +0530

    Time: 8 ms (16.93%), Space: 13.5 MB (16.16%) - LeetHub

---
+++ 0002-shuffle-the-array/0002-shuffle-the-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2 * n);
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
